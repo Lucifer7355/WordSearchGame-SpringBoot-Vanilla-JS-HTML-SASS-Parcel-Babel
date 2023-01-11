1a89bdd2f01c9b31c4f52086ee154ebf4b5ff34c
Ankit <ankitviddya@gmail.com>
2023-01-11 18:17:15 +0530

    created wordsearch full stack project

---
+++ backend/src/main/java/com/example/check/controllers/WordSearchController.java
@@ -0,0 +1,33 @@
+package com.example.check.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.check.services.WordGridService;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController("/")
+public class WordSearchController {
+
+    @Autowired
+    WordGridService wordGridService;
+
+    @GetMapping("/wordgrid")
+    @CrossOrigin(origins = "http://localhost:1234")
+    public String createWordGrid(@RequestParam int gridSize, @RequestParam String wordList) {
+        List<String> words = Arrays.asList(wordList.split(","));
+        char[][] grid = wordGridService.generateGrid(gridSize, words);
+        String gridToString = "";
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                gridToString += grid[i][j] + " ";
+            }
+            gridToString += "\r\n";
+        }
+        return gridToString;
+    }
+
+}
